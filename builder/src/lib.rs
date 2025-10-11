a91a3a664ccb464819a392973c38a5c129f6e7c3
Yihai Lin <yihai-lin@foxmail.com>
2025-10-11 17:55:22 +0800

    builder 06-optional-field
    Signed-off-by: Yihai Lin <yihai-lin@foxmail.com>

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DataStruct, DeriveInput, Fields, FieldsNamed, Type};
+use syn::{
+    parse_macro_input, DataStruct, DeriveInput, Fields, FieldsNamed, PathSegment, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -10,26 +12,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     // Get fields and ty from named struct.
     // The difficulty for newbie is the unfamiliar with the syn struct.
-    let fields = match fields {
+    let fields: Vec<OptionalField> = match fields {
         syn::Data::Struct(DataStruct {
             fields: Fields::Named(FieldsNamed { named, .. }),
             ..
         }) => named
             .iter()
             .map(|f| {
-                (
+                OptionalField::from((
                     f.ident.clone().expect("Only support named struct"),
                     f.ty.clone(),
-                )
+                ))
             })
-            .collect::<Vec<(_, _)>>(),
+            .collect(),
         _ => {
             panic!("Builder can only be derived for structs");
         }
     };
     let build_op_fields = fields
         .iter()
-        .map(|(name, ty)| {
+        .map(|op| {
+            let (name, ty) = (&op.name, &op.ty);
             // Return another TokenStream from quote! so then it can be used in the quote! macro also.
             quote! {
                 #name: ::core::option::Option<#ty>
@@ -38,7 +41,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         .collect::<Vec<_>>();
     let build_fileds = fields
         .iter()
-        .map(|(name, _)| {
+        .map(|op| {
+            let name = &op.name;
             quote! {
                 #name: None
             }
@@ -48,9 +52,16 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let set_fields_methods = set_fields(&fields);
     let set_fields_back = fields
         .iter()
-        .map(|(name, _)| {
-            quote! {
-                #name: self.#name.take().expect(&format!("Field {} is not set", stringify!(#name)))
+        .map(|op| {
+            let (name, optional) = (&op.name, &op.optional);
+            if *optional {
+                quote!{
+                    #name: self.#name.take()
+                }
+            } else {
+                quote!{
+                    #name: self.#name.take().expect(&format!("Field {} is not set", stringify!(#name)))
+                }
             }
         })
         .collect::<Vec<_>>();
@@ -84,10 +95,44 @@ pub fn derive(input: TokenStream) -> TokenStream {
     proc_macro::TokenStream::from(expanded)
 }
 
-fn set_fields(fiedls: &[(proc_macro2::Ident, Type)]) -> Vec<proc_macro2::TokenStream> {
+struct OptionalField {
+    name: proc_macro2::Ident,
+    ty: Type,
+    optional: bool,
+}
+
+impl From<(proc_macro2::Ident, Type)> for OptionalField {
+    fn from((name, ty): (proc_macro2::Ident, Type)) -> Self {
+        let (ty, optional) = parse_ty(ty);
+        OptionalField { name, ty, optional }
+    }
+}
+
+// For 06-option test, we need to parse the syntax tree of `ty` to determine whether it is optional
+fn parse_ty(ty: Type) -> (Type, bool) {
+    match ty {
+        Type::Path(TypePath {
+            path: syn::Path { ref segments, .. },
+            ..
+        }) => match segments.iter().next() {
+            Some(PathSegment {
+                ident,
+                arguments: syn::PathArguments::AngleBracketed(ref args),
+            }) if ident == "Option" => match args.args.iter().next() {
+                Some(syn::GenericArgument::Type(ref ty)) => (ty.clone(), true),
+                _ => (ty, false),
+            },
+            _ => (ty, false),
+        },
+        _ => (ty, false),
+    }
+}
+
+fn set_fields(fiedls: &[OptionalField]) -> Vec<proc_macro2::TokenStream> {
     fiedls
         .iter()
-        .map(|(name, ty)| {
+        .map(|op| {
+            let (name, ty) = (&op.name, &op.ty);
             quote! {
                 fn #name (&mut self, #name: #ty) -> &mut Self {
                     self.#name = ::core::option::Option::Some(#name);
