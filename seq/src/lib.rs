423a3385170155775bcf21fe46e5303cf720ab2f
Yihai Lin <yihai-lin@foxmail.com>
2025-10-22 11:12:51 +0800

    seq 02-parse-body
    Signed-off-by: Yihai Lin <yihai-lin@foxmail.com>

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,6 +1,38 @@
 use proc_macro::TokenStream;
 
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    parse_macro_input, ExprRange, Ident, Result, Token,
+};
+
 #[proc_macro]
-pub fn seq(_: TokenStream) -> TokenStream {
-    proc_macro::TokenStream::new()
+pub fn seq(input: TokenStream) -> TokenStream {
+    proc_macro::TokenStream::new();
+
+    let _ = parse_macro_input!(input as SeqParese);
+
+    TokenStream::new()
+}
+
+struct SeqParese {
+    _name: Ident,
+    _range: ExprRange,
+    _body: proc_macro2::TokenStream,
+}
+
+impl Parse for SeqParese {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let _name = input.parse::<Ident>()?;
+        input.parse::<Token![in]>()?;
+        let _range = input.parse::<ExprRange>()?;
+        let _body;
+        braced! {_body in input};
+
+        Ok(SeqParese {
+            _name,
+            _range,
+            _body: _body.parse()?,
+        })
+    }
 }
