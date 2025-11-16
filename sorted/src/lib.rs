84dc3b93f1a2cdd12024a8d28af8320c0c743c08
Yihai Lin <yihai-lin@foxmail.com>
2025-11-16 19:43:56 +0800

    sorted 01-parse-enum
    Signed-off-by: Yihai Lin <yihai-lin@foxmail.com>

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,10 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
+    let c = input.clone();
+    let _ = parse_macro_input!(c as Item);
 
-    unimplemented!()
+    input
 }
