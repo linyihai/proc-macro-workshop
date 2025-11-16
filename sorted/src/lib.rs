d35508b024649ddf122f5db4ae8459e3eaa868aa
Yihai Lin <yihai-lin@foxmail.com>
2025-11-16 23:01:39 +0800

    sorted 02-not-enum
    Signed-off-by: Yihai Lin <yihai-lin@foxmail.com>

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,10 +1,26 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, Item};
+use proc_macro2::Span;
+use quote::ToTokens;
+use syn::{parse_macro_input, Error, Item};
 
 #[proc_macro_attribute]
-pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
-    let c = input.clone();
-    let _ = parse_macro_input!(c as Item);
+pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(input as Item);
 
-    input
+    match sorted_impl(args, item) {
+        Ok(tokens) => tokens,
+        Err(e) => e.to_compile_error().into(),
+    }
+}
+
+fn sorted_impl(_: TokenStream, item: Item) -> Result<TokenStream, Error> {
+    if !matches!(item, Item::Enum(_)) {
+        // Not `item.span`
+        return Err(Error::new(
+            Span::call_site(),
+            "expected enum or match expression",
+        ));
+    };
+
+    Ok(TokenStream::from(item.to_token_stream()))
 }
