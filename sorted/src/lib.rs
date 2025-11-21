8b05d3de35625d6ad5de9ecbe2e6bf7e7d6b914f
Yihai Lin <yihai-lin@foxmail.com>
2025-11-21 22:40:12 +0800

    sorted 04-variants-with-data
    Signed-off-by: Yihai Lin <yihai-lin@foxmail.com>

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -7,21 +7,17 @@ use syn::{parse_macro_input, Error, Item};
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as Item);
 
-    match sorted_impl(args, item) {
-        Ok(tokens) => tokens,
-        Err(e) => e.to_compile_error().into(),
-    }
+    sorted_impl(args, item)
 }
 
-fn sorted_impl(_: TokenStream, item: Item) -> Result<TokenStream, Error> {
+fn sorted_impl(_: TokenStream, item: Item) -> TokenStream {
     let Item::Enum(enum_item) = item else {
         // Not `item.span`
-        return Err(Error::new(
-            Span::call_site(),
-            "expected enum or match expression",
-        ));
+        return Error::new(Span::call_site(), "expected enum or match expression")
+            .to_compile_error()
+            .into();
     };
-
+    let mut tokens = TokenStream::from(enum_item.to_token_stream());
     for (i, v) in enum_item.variants.iter().enumerate() {
         if i == 0 {
             continue;
@@ -32,16 +28,19 @@ fn sorted_impl(_: TokenStream, item: Item) -> Result<TokenStream, Error> {
         }
         for j in enum_item.variants.iter().take(i) {
             if j.ident > v.ident {
-                return Err(Error::new(
+                let err_token: TokenStream = Error::new(
                     v.ident.span(),
-                    format!(
-                        "{} should sort before {}",
-                        v.ident, j.ident
-                    ),
-                ));
+                    format!("{} should sort before {}", v.ident, j.ident),
+                )
+                .to_compile_error()
+                .into();
+                // sorted 宏不能直接返回错误，这样子是的没有使用到use导入的包，这里的
+                // 解决方法是将错误作为Tokenstream返回
+                tokens.extend(err_token);
+                return tokens;
             }
         }
     }
 
-    Ok(TokenStream::from(enum_item.to_token_stream()))
+    tokens
 }
